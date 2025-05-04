d470276ad6664317c0e5d4cec443b0ea0bead78e
ambuj vashistha <ambujva123@gmail.com>
2025-05-04 21:55:02 +0530

    made a draggable component

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-
+import React,{useState} from "react";
+import {Rnd} from 'react-rnd'
 export default function Canvas(){
-
+    const [rnd,setRnd]=useState({width:"100px",height:"100px",x:10,y:10})
     return(
-        <>
+        <div>
+            <Rnd 
+            style={{backgroundColor: "red"}}
+            size={{width:rnd.width, height:rnd.height}}
+            position={{x:rnd.x,y:rnd.y}}
+            >
 
-        </>
+            </Rnd>
+            </div>
     )
 }
