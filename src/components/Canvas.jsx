39b9328edb766ed2ce93f1875b5f75cda168794f
ambuj vashistha <ambujva123@gmail.com>
2025-05-04 22:46:23 +0530

    made a resizable component

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,12 +2,24 @@ import React,{useState} from "react";
 import {Rnd} from 'react-rnd'
 export default function Canvas(){
     const [rnd,setRnd]=useState({width:"100px",height:"100px",x:10,y:10})
+    const setPositions=(e,direction)=>{
+        setRnd(
+            prevRnd=>({...prevRnd,x:direction.x,y:direction.y})
+        )
+    }
+    const setSize=(e,direction,ref,delta)=>{
+        setRnd(
+            prevRnd=>({...prevRnd,width:parseInt(ref.style.width),height:parseInt(ref.style.height)})
+        )
+    }
     return(
         <div>
             <Rnd 
             style={{backgroundColor: "red"}}
             size={{width:rnd.width, height:rnd.height}}
             position={{x:rnd.x,y:rnd.y}}
+            onDragStop={setPositions}
+            onResizeStop={setSize}
             >
 
             </Rnd>
